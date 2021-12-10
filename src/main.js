3fd551c6c89ce032e9620f2ac5c97870d31e47e5
ozanulutas <oznulutas@gmail.com>
2021-12-11 01:48:56 +0300

    feat: add vuelidate library

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { BootstrapVue } from 'bootstrap-vue'
 
+import i18n from './i18n'
+import Vuelidate from 'vuelidate'
+import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import i18n from './i18n'
 
+Vue.use(Vuelidate)
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
